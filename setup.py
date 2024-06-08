8aeb04aa5fc6e4df34a71dff1303badd44943127
KavyanshKhaitan2 <73186427+KavyanshKhaitan2@users.noreply.github.com>
2024-06-08 15:19:25 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ PATH = pathlib.Path(__file__).parent
 README = (PATH / "README.md").read_text()
 
 setup(
-	name = 'overlay',
+	name = 'py-tkoverlay',
 	version = '1.1.0',
 	description = 'A package that creates and manipulates screen overlays based on tkinter.',
 	long_description = README,
 	long_description_content_type = "text/markdown",
 	url = 'https://github.com/davidmaamoaix/Overlay',
-	author = 'David Ma',
-	author_email = "davidma@davidma.cn",
+	author = 'Kavyansh Khaitan',
+	author_email = "kavyanshkhaitan11@gmail.com",
 	license = 'MIT',
 	packages = find_packages(),
 	include_package_data = True,
